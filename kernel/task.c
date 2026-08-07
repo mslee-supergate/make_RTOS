6f2a81bb64706027b73c9d6b44378be188c6b435
mslee <ms.lee@supergate.cc>
2026-08-07 10:35:04 +0900

    Implement RoundRobin Scheduler

--- kernel/task.c
+++ kernel/task.c
@@ -7,6 +7,9 @@
 static KernelTcb_t sTask_list[MAX_TASK_NUM];
 static uint32_t    sAllocated_tcb_index;
 
+static uint32_t sCurrent_tcp_index;
+static KernelTcb_t* Scheduler_round_robin_algorithm(void);
+
 void Kernel_task_init(void)
 {
     sAllocated_tcb_index = 0;
@@ -38,3 +41,12 @@ uint32_t Kernel_task_create(KernelTaskFunc_t startFunc)
 
     return task_id;
 }
+
+static KernelTcb_t* Scheduler_round_robin_algorithm(void)
+{
+    sCurrent_tcp_index++;
+    sCurrent_tcp_index %= sAllocated_tcb_index;
+
+    return &sTask_list[sCurrent_tcp_index];
+}
+
